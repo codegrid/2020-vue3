b2359b3f8cc8258a894f5e9dd7e904ca171b7c73
cyokodog <cyokodog@gmail.com>
2021-02-05 17:39:14 +0900

    第４回デモ追加

---
+++ 04/todo_04/index.js
@@ -0,0 +1,19 @@
+const Todo = {
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: '野菜を買う', done: false },
+        { id: 2, title: '魚を買う', done: false },
+        { id: 3, title: 'JavaScriptの勉強をする', done: false },
+        { id: 4, title: '原稿を書く', done: false },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter(task => task.id !== taskId);
+    },
+  },
+};
+const todoApp = Vue.createApp(Todo);
+todoApp.mount('#todoApp');
