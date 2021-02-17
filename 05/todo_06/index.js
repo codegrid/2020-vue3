fae50d25a401bcbb732545b7195d2c2e536de40c
cyokodog <cyokodog@gmail.com>
2021-02-17 15:52:00 +0900

    第５回デモ追加

---
+++ 05/todo_06/index.js
@@ -0,0 +1,44 @@
+const DONE_FILTER = {
+  DONE: true,
+  UNDONE: false,
+  ALL: null,
+};
+const Todo = {
+  data() {
+    return {
+      doneFilter: DONE_FILTER.UNDONE,
+      titleFilter: '',
+      tasks: [
+        { id: 1, title: '野菜を買う', done: false },
+        { id: 2, title: '魚を買う', done: false },
+        { id: 3, title: 'JavaScriptの勉強をする', done: false },
+        { id: 4, title: '原稿を書く', done: false },
+      ],
+    };
+  },
+  computed: {
+    DONE_FILTER() {
+      return Vue.readonly(DONE_FILTER);
+    },
+    filteredTasks() {
+      if (this.titleFilter === '' && this.doneFilter === DONE_FILTER.ALL) {
+        return this.tasks;
+      }
+      return this.tasks.filter(task => {
+        const titleMatched = this.titleFilterExp.test(task.title);
+        const doneMatched = [DONE_FILTER.ALL, task.done].some(v => v === this.doneFilter);
+        return titleMatched && doneMatched;
+      });
+    },
+    titleFilterExp() {
+      return new RegExp(this.titleFilter);
+    },
+  },
+  methods: {
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter(task => task.id !== taskId);
+    },
+  },
+};
+const todoApp = Vue.createApp(Todo);
+todoApp.mount('#todoApp');
